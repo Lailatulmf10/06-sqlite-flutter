4a63fd37b592b6dbd339f4eab88c20f9091f8708
Lailatul <mufidalailatul10@gmail.com>
2022-04-05 05:30:55 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:sqlite_flutter/pages/home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tambahkan Item => Lailatul Mufida | 2031710094',
+      title: 'Tambahkan Item',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: Home(),
     );
   }
 }
+ 
